2bf2ee7a50a3081b0ce90f8772cb89321b781ec9
Stjepan Horvat <StjepanH@teched.hr>
2020-08-21 12:00:31 +0200

    End of section 13

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext, useEffect } from "react";
 import { Container } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import { observer } from "mobx-react-lite";
@@ -14,12 +14,34 @@ import ActivityDashboard from "../../features/activities/dashboard/ActivityDashb
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import NotFound from "./NotFound";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
+import { LoginForm } from "../../features/user/LoginForm";
+import { RootStoreContext } from "../stores/rootStore";
+import { LoadingComponent } from "./LoadingComponent";
+import ModalContainer from "../common/modals/ModalContainer";
+import { RegisterForm } from "../../features/user/RegisterForm";
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
+  const rootStore = useContext(RootStoreContext);
+  const { setAppLoaded, token, appLoaded } = rootStore.commonStore;
+  const { getUser } = rootStore.userStore;
+
+  useEffect(() => {
+    if (token) {
+      getUser().finally(() => {
+        setAppLoaded();
+      });
+    } else {
+      setAppLoaded();
+    }
+  }, [getUser, setAppLoaded, token]);
+
+  if (!appLoaded) return <LoadingComponent content="Loading app..." />;
+
   return (
     <Fragment>
-      <ToastContainer position='bottom-right' />
+      <ModalContainer />
+      <ToastContainer position="bottom-right" />
       <Route exact path="/" component={HomePage} />
       <Route
         path={"/(.+)"}
@@ -27,7 +49,9 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
           <Fragment>
             <NavBar />
             <Container style={{ marginTop: "7em" }}>
-              <Switch> {/* switch makes sure that only one of the routes is always shown */}
+              <Switch>
+                {" "}
+                {/* switch makes sure that only one of the routes is always shown */}
                 <Route exact path="/" component={HomePage} />
                 <Route exact path="/activities" component={ActivityDashboard} />
                 <Route path="/activities/:id" component={ActivityDetails} />
@@ -36,6 +60,8 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
                   path={["/createActivity", "/manage/:id"]}
                   component={ActivityForm}
                 />
+                <Route path="/login" component={LoginForm} />
+                <Route path="/register" component={RegisterForm} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
